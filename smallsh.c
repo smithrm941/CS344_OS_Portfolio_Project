6fd83947f33920560d993f56be194b17be72c877
Rhonda Smith <smithrh@os1.engr.oregonstate.edu>
2021-10-31 18:58:37 -0700

    getting user input with spaces and tokenizing

--- smallsh.c
+++ smallsh.c
@@ -10,21 +10,49 @@ int main() {
 	char cwd[256];
 	int status;
 
+	struct userCommand
+	{
+		char* command;
+		char* inputFile;
+		char* outputFile;
+		char* background;
+	};
+
+
 	while (1) {
+		struct userCommand* currVal = malloc(sizeof(struct userCommand));
+
 		printf(": ");
-		scanf("%s", userInput);
-		if (strcmp(userInput, "exit") == 0) {
+		scanf("%[^\n]%*c", userInput);
+
+		// user command is first argument user typed:
+		char* token;
+		token = strtok(userInput, " ");
+		
+		// exit smallsh if command is 'exit':
+		if (strcmp(token, "exit") == 0) {
 			exit(0);
 		}
-		else if (strcmp(userInput, "cd") == 0) {
-			/* getcwd(cwd, 256);
-			printf("current directory: %s\n", getcwd(cwd, 256));
-			printf("home directory is: %s\n", getenv("HOME")); */
-			chdir(getenv("HOME"));
-			// printf("current directory: %s\n", getcwd(cwd, 256));
-
+		// if the first argument is 'cd' either change directory to chosen directory
+		// or home if no directory is chosen:
+		else if (strcmp(token, "cd") == 0) {
+			token = strtok(NULL, " ");
+			// if a directory is chosen:
+			while (token != NULL) {
+				printf("the token: %s\n", token);
+				token = strtok(NULL, " ");
+			} 
+			// if cd is typed without chosen directory:
+			    getcwd(cwd, 256);
+			    //printf("current directory: %s\n", getcwd(cwd, 256));
+				//printf("home directory is: %s\n", getenv("HOME")); 
+				chdir(getenv("HOME"));
+				//printf("current directory: %s\n", getcwd(cwd, 256));
 		}
-		else if (strcmp(userInput, "status") == 0) {
+
+		else if (strcmp(token, "status") == 0) {
+			// PLACEHOLDER - signal of last terminating process here
+			// otherwise if before any foreground process is run:
 			status = 0;
 			printf("exit value %d\n", status);
 			exit(0);
