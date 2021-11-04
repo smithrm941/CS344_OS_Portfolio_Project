ad37020391dc235829f2768e72a491c2a2255ec7
Rhonda Smith <smithrh@os1.engr.oregonstate.edu>
2021-11-04 03:54:47 -0700

    mostly functional, need to add comments

--- smallsh.c
+++ smallsh.c
@@ -14,17 +14,18 @@ void handle_SIGINT(int signo) {
 	printf("\n");
 }
 
+void handle_SIGTSTP(int signo) {
+	printf("do something!");
+}
+
 
 struct userCommands {
 	char inputFile[256];
 	char outputFile[256];
 	char* regularCommands[MAXARGS];
 	int commandCount;
-	int backgroundProcess;
-	//int i;
-/*	for (i = 0; i < userCommands.commandCount; i++) {
-		userCommands.regularCommands[i] = 0;
-	}*/
+//	int backgroundProcess;
+
 };
 
 
@@ -34,6 +35,9 @@ struct userCommands {
 // and Dy Classroom: dyclassroom.com/c/c-function-returning-structure
 struct userCommands processInput(void);
 void executeCommand(struct userCommands);
+int status = 0; // var for exit status
+
+//void handle_SIG
 
 
 int main() {
@@ -46,8 +50,13 @@ int main() {
 	sigaction(SIGINT, &SIGINT_action, NULL);
 
 
-	char command[256];
-	char backgroundProcess[256];
+	// Ignoring CTRL Z in smallsh 
+	// code from signal handling API class module 
+	struct sigaction SIGTSTP_action = { 0 };
+	SIGTSTP_action.sa_handler = handle_SIGTSTP;
+	sigfillset(&SIGTSTP_action.sa_mask);
+	SIGTSTP_action.sa_flags = 0;
+	sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
 
 	struct userCommands theCommands = processInput();
@@ -72,7 +81,6 @@ struct userCommands processInput(void) {
 		char regularCommands[MAXARGS][256];
 
 		char cwd[256]; // var to hold current working directory
-		int status; // var for exit status
 		char* newDirectory; // var for usr chosen directory
 		pid_t spawnpid = -5;
 		char* token;
@@ -98,6 +106,7 @@ struct userCommands processInput(void) {
 		// exit shell if user types "exit"
 		else if (strcmp(userInput, "exit") == 0) {
 			exit(0);
+			break;
 		}
 
 		// print status value if user types "status"
@@ -114,6 +123,7 @@ struct userCommands processInput(void) {
 
 			// breaking user input into tokens seperated by space
 			token = strtok(userInput, " ");
+			//token[strlen(token) - 1] = 0;
 			while (token != NULL) {
 
 				// built-in function for changing directory within smallsh:
@@ -129,6 +139,7 @@ struct userCommands processInput(void) {
 							main();
 						}
 						else {
+
 							printf("Directory not found\n");
 							main();
 						}
@@ -162,18 +173,36 @@ struct userCommands processInput(void) {
 							strcpy(currentCommands.outputFile, token);
 							printf("the output file: %s\n", currentCommands.outputFile);
 						}
-						else if (strcmp(token, "&") == 0) {
+						/*else if (strcmp(token, "&") == 0) {
 							printf("do the background thing!");
 							currentCommands.backgroundProcess = 1;
 							token = strtok(NULL, " ");
-						}
+						}*/
 						else {
 						//	printf("just a command!\n");
 							currentCommands.regularCommands[currentCommands.commandCount] = token;
 							currentCommands.commandCount++;
 						}
-						
+						int i;
+						int parentPid = getpid();
+						char parentPidStr[256];
+						sprintf(parentPidStr, "%d", parentPid);
+						int usedPidVariable = 0;
+						//char* parentPidStr = sprintf("%s", parentPid);
+						for (i = 0; i < strlen(token); i++) {
+							//printf("letter from token: %c\n", token[i]);
+							if (token[i] == '$' && token[i + 1] == '$') {
+								token[i + 1] = '\0';
+								token[i] = '\0';
+								usedPidVariable = 1;
+							}
+						}
+						if (usedPidVariable == 1) {
+							strcat(token, parentPidStr);
+							token = strtok(NULL, " ");
+						}
 						token = strtok(NULL, " ");
+
 						
 					}
 					
@@ -205,6 +234,8 @@ void executeCommand(struct userCommands theCommands) {
 			int sourceFD = open(theCommands.inputFile, O_RDONLY);
 			if (sourceFD == -1) {
 				perror("source open()");
+				status = 1;
+				printf("exit value %d\n", status);
 				exit(1);
 			}
 			// Written to terminal
@@ -214,6 +245,8 @@ void executeCommand(struct userCommands theCommands) {
 			int result = dup2(sourceFD, 0);
 			if (result == -1) {
 				perror("source dup2()");
+				status = 2;
+				printf("exit value %d\n", status);
 				exit(2);
 			}
 		}
@@ -224,6 +257,8 @@ void executeCommand(struct userCommands theCommands) {
 			int targetFD = open(theCommands.outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (targetFD == -1) {
 				perror("target open()");
+				status = 1;
+				printf("exit value %d\n", status);
 				exit(1);
 			}
 			printf("targetFD == %d\n", targetFD); // written to terminal
@@ -232,11 +267,15 @@ void executeCommand(struct userCommands theCommands) {
 			int result = dup2(targetFD, 1);
 			if (result == -1) {
 				perror("target dup2()");
+				status = 2;
+				printf("exit value %d\n", status);
 				exit(2);
 			}
 			
 		}
 		execvp(theCommands.regularCommands[0], theCommands.regularCommands);
+		perror("execvp()");
+		exit(1);
 		//fflush(stdout);
 	default:
 		// in parent process, wait for child's termination:
